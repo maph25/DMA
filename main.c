72a0f9d0e1e5be03b23569a87ab51539e139dce6
Andrea Perez Huizar <39924790+maph25@users.noreply.github.com>
2018-10-10 14:58:30 -0500

    Correction and functioning

--- main.c
+++ main.c
@@ -1,22 +1,20 @@
-
-
 #include "MK64F12.h"
 #include "NVIC.h"
 #include "GPIO.h"
 #include "Bits.h"
 #include <stdio.h>
 
-#define NUM_STEPS (2u)
+#define NUM_STEPS (1u)
 #define SYSTEM_CLOCK (21000000u)
 #define DELAY (0.01F)
 
 #define ARRAY_SIZE (16u)
 
 #define DMA_CH0 (0x01u)
-#define DMA_SOURCE_GPIO (51u) /*Port c*/
+#define DMA_SOURCE_GPIO (51u)
 
-uint8_t g_data_source[ARRAY_SIZE] = {1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16};//defines source data space
-uint8_t g_data_desti[ARRAY_SIZE]; //defines destination data space
+uint16_t g_data_source[ARRAY_SIZE] = {1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16};//defines source data space
+uint16_t g_data_desti[ARRAY_SIZE]; //defines destination data space
 
 
 void DMA0_IRQHandler(void)
@@ -50,21 +48,17 @@ void DMA_init(void)
 
 	DMA0->ERQ = 0x01;//enables DMA0 request
 
-	DMA0->TCD[0].SADDR = (uint32_t)(&g_data_source[0]);/*defines source data address*/
-	DMA0->TCD[0].SOFF = 2;/*Source address signed offset; it is expressed in number of bytes
-	 	 	 	 	 	 	 when  a transfer occurs the pointer moves one byte, how many
-	 	 	 	 	 	 	 address spaces does the pointer move*/
+	DMA0->TCD[0].SADDR = (uint32_t)(&g_data_source[4]);/*defines source data address*/
+	DMA0->TCD[0].SOFF = 4;/*Source address signed offset;it is expressed in number of bytes*/
 	DMA0->TCD[0].DADDR = (uint32_t)(&g_data_desti[0]);/*defines destination data address*/
-	DMA0->TCD[0].DOFF = 5;/*destination address signed offset;it is expressed in number of bytes
-	 	 	 	 	 	 	 writes in the destination*/
+	DMA0->TCD[0].DOFF = 2;/*destination address signed offset;it is expressed in number of bytes*/
 
-	DMA0->TCD[0].CITER_ELINKNO = DMA_CITER_ELINKNO_CITER(NUM_STEPS);// NUM_STEPS;/*CITER = 1 minor loop*/
-	DMA0->TCD[0].BITER_ELINKNO = DMA_BITER_ELINKNO_BITER(NUM_STEPS);/*BITER = 1 major loop*/
-	/*CITER and BITER must be the same to create a proper loop*/
+	DMA0->TCD[0].CITER_ELINKNO = DMA_CITER_ELINKNO_CITER(NUM_STEPS);// NUM_STEPS;/*CITER = 1*/
+	DMA0->TCD[0].BITER_ELINKNO = DMA_BITER_ELINKNO_BITER(NUM_STEPS);/*BITER = 1*/
 	DMA0->TCD[0].NBYTES_MLNO = 3;/*byte number*/
 
 	DMA0->TCD[0].ATTR = 0;/*8 bit transfer size, in order to transfer see Kinetis user manual*/
-	DMA0->TCD[0].SLAST = -9;//restores the source address to the initial value, which is expressed in the amount of bytes to restore*/
+	DMA0->TCD[0].SLAST = -20;//restores the source address to the initial value, which is expressed in the amount of bytes to restore*/
 	DMA0->TCD[0].DLAST_SGA = 0;/*restores the destination address to the initial value, which is expressed in the amount of bytes to restore*/
 	DMA0->TCD[0].CSR = DMA_CSR_INTMAJOR_MASK;/*The end-of-major loop interrupt is enabled*/
 
@@ -90,3 +84,4 @@ int main(void)
 ////////////////////////////////////////////////////////////////////////////////
 // EOF
 ////////////////////////////////////////////////////////////////////////////////
+
